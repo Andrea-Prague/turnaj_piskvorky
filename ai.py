b1b5478a6207c93b563d069749fab488c5f2d4b4
Andrea Lisa <andrea.lisaa@gmail.com>
2018-11-26 20:02:08 +0100

    Rozšíření strategie

--- ai.py
+++ ai.py
@@ -12,17 +12,46 @@ def tah_pocitace(pole, symbol):
     mezera_mezi = [symbol, "-", symbol]
     str_mezera_mezi = "".join(mezera_mezi)
 
-    if str_jeden_symbol in pole:
-        pc_cislo_pole = pole.index(str_jeden_symbol)+1
+    if not "x" in symbol and "x" in pole:
+        if str_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_jeden_symbol)+1
 
-    elif str_za_jeden_symbol in pole:
-        pc_cislo_pole = pole.index(str_za_jeden_symbol)
+        elif str_za_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_za_jeden_symbol)
 
-    elif str_mezera_mezi in pole:
-        pc_cislo_pole = pole.index(str_mezera_mezi)
+        elif str_mezera_mezi in pole:
+            pc_cislo_pole = pole.index(str_mezera_mezi)
 
-    elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
-        while not "-" in pole[pc_cislo_pole]:
+        elif "xx-" in pole:
+            pc_cislo_pole = pole.index("xx-")+2
+
+        elif "-xx" in pole:
+            pc_cislo_pole = pole.index("-xx")+2
+
+        elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
             pc_cislo_pole = randrange(len(pole))
+        while not "-" in pole[pc_cislo_pole]:
+                pc_cislo_pole = randrange(len(pole))
+
+    elif not "o" in symbol and "o" in pole:
+        if str_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_jeden_symbol)+1
+
+        elif str_za_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_za_jeden_symbol)
+
+        elif str_mezera_mezi in pole:
+            pc_cislo_pole = pole.index(str_mezera_mezi)
+
+        elif "oo-" in pole:
+            pc_cislo_pole = pole.index("oo-")+2
+
+        elif "-oo" in pole:
+            pc_cislo_pole = pole.index("-oo")+2
+
+        elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
+            pc_cislo_pole = randrange(len(pole))
+        while not "-" in pole[pc_cislo_pole]:
+                pc_cislo_pole = randrange(len(pole))
 
     return tah (pole, pc_cislo_pole, symbol)
