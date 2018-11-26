712b951081475fbccff34f448ddd0b1a20f75659
Andrea Lisa <andrea.lisaa@gmail.com>
2018-11-26 19:23:44 +0100

    update 2.0

--- piskovrky_vylepseny_pc_rozprac.py
+++ piskovrky_vylepseny_pc_rozprac.py
@@ -27,29 +27,59 @@ def tah_hrace (pole, symbol):
 
 def tah_pocitace(pole, symbol):
     pc_cislo_pole= randrange(len(pole))
-    jeden_symbol = [symbol, "-", "-"]
+    jeden_symbol = [symbol, "-"]
     str_jeden_symbol = "".join(jeden_symbol)
 
-    za_jeden_symbol =  ["-","-", symbol]
+    za_jeden_symbol =  ["-", symbol]
     str_za_jeden_symbol = "".join(za_jeden_symbol)
 
     mezera_mezi = [symbol, "-", symbol]
     str_mezera_mezi = "".join(mezera_mezi)
 
-    if str_jeden_symbol in pole:
-        pc_cislo_pole = pole.index(str_jeden_symbol)+1
+    if not "x" in symbol and "x" in pole:
+        if str_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_jeden_symbol)+1
 
-    # elif str_za_jeden_symbol in pole:
-    #     pc_cislo_pole = pole.index(str_za_jeden_symbol)
+        elif str_za_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_za_jeden_symbol)
 
-    elif str_mezera_mezi in pole:
-        pc_cislo_pole = pole.index(str_mezera_mezi)
+        elif str_mezera_mezi in pole:
+            pc_cislo_pole = pole.index(str_mezera_mezi)
 
-    elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
-        while not "-" in pole[pc_cislo_pole]:
+        elif "xx-" in pole:
+            pc_cislo_pole = pole.index("xx-")+2
+
+        elif "-xx" in pole:
+            pc_cislo_pole = pole.index("-xx")+2
+
+        elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
             pc_cislo_pole = randrange(len(pole))
+        while not "-" in pole[pc_cislo_pole]:
+                pc_cislo_pole = randrange(len(pole))
+
+    elif not "o" in symbol and "o" in pole:
+        if str_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_jeden_symbol)+1
+
+        elif str_za_jeden_symbol in pole:
+            pc_cislo_pole = pole.index(str_za_jeden_symbol)
+
+        elif str_mezera_mezi in pole:
+            pc_cislo_pole = pole.index(str_mezera_mezi)
+
+        elif "oo-" in pole:
+            pc_cislo_pole = pole.index("oo-")+2
+
+        elif "-oo" in pole:
+            pc_cislo_pole = pole.index("-oo")+2
+
+        elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
+            pc_cislo_pole = randrange(len(pole))
+        while not "-" in pole[pc_cislo_pole]:
+                pc_cislo_pole = randrange(len(pole))
 
     return tah (pole, pc_cislo_pole, symbol)
+
 
 def piskovrky1d():
     pole = "-" * 20
