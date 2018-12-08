d7e25ced1e9f7375f8d781c6d877a7ef94fa7f63
Andrea Lisa <andrea.lisaa@gmail.com>
2018-12-08 18:15:23 +0100

    PyLadies turnaj ve strategii pocitace - file strategie, pak puvodni soubor celeho programu bez strategie

--- puvodni_piskvorky_cele.py
+++ puvodni_piskvorky_cele.py
@@ -25,31 +25,11 @@ def tah_hrace (pole, symbol):
         else:
             return tah(pole, cislo_pole,symbol)
 
-def tah_pocitace(pole, symbol):
-    pc_cislo_pole= randrange(len(pole))
-    jeden_symbol = [symbol, "-"]
-    str_jeden_symbol = "".join(jeden_symbol)
-
-    za_jeden_symbol =  ["-", symbol]
-    str_za_jeden_symbol = "".join(za_jeden_symbol)
-
-    mezera_mezi = [symbol, "-", symbol]
-    str_mezera_mezi = "".join(mezera_mezi)
-
-    if str_jeden_symbol in pole:
-        pc_cislo_pole = pole.index(str_jeden_symbol)+1
-
-    elif str_za_jeden_symbol in pole:
-        pc_cislo_pole = pole.index(str_za_jeden_symbol)
-
-    elif str_mezera_mezi in pole:
-        pc_cislo_pole = pole.index(str_mezera_mezi)
-
-    elif not str_jeden_symbol in pole or not str_za_jeden_symbol in pole:
-        while not "-" in pole[pc_cislo_pole]:
-            pc_cislo_pole = randrange(len(pole))
-
-    return tah (pole, pc_cislo_pole, symbol)
+def tah_pocitace(pole):
+    pc_cislo_pole = randrange(len(pole))
+    while not "-" in pole[pc_cislo_pole]:
+        pc_cislo_pole = randrange(len(pole))
+    return tah (pole, pc_cislo_pole, "o")
 
 def piskovrky1d():
     pole = "-" * 20
@@ -62,7 +42,7 @@ def piskovrky1d():
                 print ("uzivatel vyhral")
                 break
 
-            pole = tah_pocitace(pole, "o")
+            pole = tah_pocitace(pole)
             print (pole)
 
             if vyhodnot(pole) == "o":
